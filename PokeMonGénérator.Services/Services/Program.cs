5d314e76c8a3c1efc6fd567318bbe9969a9148ef
Louis Dhellemmes <louis.dhellemmes@altran.com>
2020-08-11 18:52:25 +0200

    initial commit

---
+++ PokeMonGénérator.Services/Services/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PokeMonGénérator.Services
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
