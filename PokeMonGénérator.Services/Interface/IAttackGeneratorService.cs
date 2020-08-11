c2e87a6ee4d64c20df1db66582c07a8e136480c5
Louis Dhellemmes <louis.dhellemmes@altran.com>
2020-08-11 20:01:20 +0200

    Lancement du recit POK1

---
+++ PokeMonGénérator.Services/Interface/IAttackGeneratorService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PokeMonGénérator.Services.Interface
+{
+    interface IAttackGeneratorService
+    {
+        string GetById(int id);
+        string GetOneRandom();
+        List<string> GetFourRandom();
+        string GetOneRandomByType(string type);
+        List<string> GetFourRandomByType(string type);
+
+    }
+}
