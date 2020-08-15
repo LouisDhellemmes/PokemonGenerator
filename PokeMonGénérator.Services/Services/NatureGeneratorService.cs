65f7a16108e30c65ff861152d257f75110162245
Clément Guerin <clement.guerin@univ-tlse3.fr>
2020-08-15 15:41:28 +0200

    POK-9

--- PokeMonGénérator.Services/Services/NatureGeneratorService.cs
+++ PokeMonGénérator.Services/Services/NatureGeneratorService.cs
@@ -9,15 +9,18 @@ namespace PokeMonGénérator.Services
     public class NatureGeneratorService
     {
         public string nature;
+
         public string GetById(int id){
             string text = File.ReadAllText("../../../../BD/nature.txt");
             string[] listNature = text.Split(' ');
-            Console.WriteLine(listNature);
+
             return nature = listNature[id*3];
         }
+
         public string GetRandom(){
             int randInt;
             Random rand = new Random();
+
             randInt = rand.Next(1, 25);
             nature = GetById(randInt);
             return nature;
